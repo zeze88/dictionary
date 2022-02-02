080d2df3c7231cef6445d2e3af0234a47b6d3354
재연 <charmant1402@gmail.com>
2022-02-02 19:54:54 +0900

    수정페이지 새로고침시 에러페이지 뜨는거 수정 여전히 데이터는 가지고오지는 못하지만 그래도 입력창은 나옴

--- src/redux/configStore.js
+++ src/redux/configStore.js
@@ -1,11 +1,9 @@
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import dictionaryList from "./modules/dictionaryList";
-import targetDic from "./modules/targetDic";
 import thunk from "redux-thunk";
 
 const middlewares = [thunk];
-
-const rootReducer = combineReducers({ dictionaryList, targetDic });
+const rootReducer = combineReducers({ dictionaryList });
 
 //optional 한 것들의 모임
 const enhancer = applyMiddleware(...middlewares);
